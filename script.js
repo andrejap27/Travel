1f9508cb506e9e37c54775eae099fdb882887d10
andrejap27 <153460423+andrejap27@users.noreply.github.com>
2026-05-15 20:09:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.getElementById('contactForm')?.addEventListener('submit', function(e) {
     alert('Hvala Vam! Vaša poruka je uspešno poslata.');
     this.reset();
 });
+
+
+  window.addEventListener('mouseover', initLandbot, { once: true });
+  window.addEventListener('touchstart', initLandbot, { once: true });
+  var myLandbot;
+  function initLandbot() {
+    if (!myLandbot) {
+      var s = document.createElement('script');
+      s.type = "module"
+      s.async = true;
+      s.addEventListener('load', function() {
+        var myLandbot = new Landbot.Livechat({
+          configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3432712-9VT4L0EZIPDH4JZO/index.json',
+        });
+      });
+      s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+  }
